c1de22e8e4144b241a7dc91cc91d856130c238ea
kiranpalsingh1806 <kiranpalsingh1806@gmail.com>
2022-07-20 22:49:24 +0530

    Add One To Many Relation in Model Schema

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    { 'id' : 1, 'name' : "Let's learn Python"},
-    { 'id' : 2, 'name' : "Web Development"},
-    { 'id' : 3, 'name' : "Ruby on Rails"},
-    { 'id' : 4, 'name' : "Flask Dev"},
-]
+# rooms = [
+#     { 'id' : 1, 'name' : "Let's learn Python"},
+#     { 'id' : 2, 'name' : "Web Development"},
+#     { 'id' : 3, 'name' : "Ruby on Rails"},
+#     { 'id' : 4, 'name' : "Flask Dev"},
+# ]
 
 def home(request) :
+    rooms = Room.objects.all()
     context = {'rooms' : rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i ['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room' : room}
     return render(request, 'base/room.html', context)
